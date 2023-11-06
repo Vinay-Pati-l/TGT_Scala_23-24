3b7ee1d69c65e356cc36f7167ddd402efd23362e
mallikagulati <20bcs3897@cuchd.in>
2023-11-06 23:47:35 +0530

    Added class and tests of Basic Math Program

---
+++ src/main/scala/mallika/BasicMathProgram.scala
@@ -0,0 +1,24 @@
+
+package Assignment1
+
+
+
+class BasicMathProgram {
+  def add(x: Int , y:Int)=
+    x+y
+
+  def subtract(x: Int, y: Int) =
+    x - y
+
+  def multiply(x: Int, y: Int) =
+    x * y
+
+  def divide(x: Int, y: Int) =
+    x / y
+
+}
+
+
+
+
+
---
+++ src/test/scala/mallika/BasicMathProgramTest.scala
@@ -0,0 +1,68 @@
+package Assignment1
+
+import org._
+import org.scalatest.funsuite.AnyFunSuite
+
+class BasicMathProgramTest extends AnyFunSuite {
+  val basicMathProgram = new BasicMathProgram()
+  test("BasicTest") {
+    assert(basicMathProgram.add(2, 3) === 5)
+  }
+
+  test("Add0") {
+    assert(basicMathProgram.add(0, 0) === 0)
+  }
+
+  test("TestNegative") {
+    assert(basicMathProgram.add(-1, -3) === -4)
+  }
+
+  test("BasicTestSubtraction") {
+    assert(basicMathProgram.subtract(3, 2) === 1)
+  }
+
+  test("Sub0") {
+    assert(basicMathProgram.subtract(8, 0) === 8)
+  }
+
+  test("TestNegativeSubtraction") {
+    assert(basicMathProgram.subtract(-1, -3) === 2)
+  }
+
+  test("BasicTestMultiplication") {
+    assert(basicMathProgram.multiply(3, 8) === 24)
+  }
+
+  test("Mul0") {
+    assert(basicMathProgram.multiply(8, 0) === 0)
+  }
+
+  test("TestNegativeMultiplication") {
+    assert(basicMathProgram.multiply(-4, -5) === 20)
+  }
+
+  test("BasicTestDivision") {
+    assert(basicMathProgram.divide(36, 12) === 3)
+  }
+
+  test("Div0") {
+    assert(basicMathProgram.divide(0, 9) === 0)
+  }
+
+  test("TestNegativeDivision") {
+    assert(basicMathProgram.divide(-45, -5) === 9)
+  }
+
+  test("Divisionby0") {
+    assertThrows[ArithmeticException] {
+      basicMathProgram.divide(6, 0)
+    }
+
+  }
+}
+
+
+
+
+
+
