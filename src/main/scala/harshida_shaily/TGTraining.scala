9f5d8bbd95d16e51f55208e816835e7004dde789
Harshida <20BCS2198@gmail.com>
2023-11-07 11:46:52 +0530

    Your commit message here

--- src/main/scala/harshida_shaily/TGTraining.scala
+++ src/main/scala/harshida_shaily/TGTraining.scala
@@ -1,6 +1,21 @@
 package harshida_shaily
 
-class TGTraining {
 
-  def welcome() = "Welcome to Scala"
+
+class TGTraining {
+  def add(a: Int, b: Int): Int = a + b
+
+  def subtract(a: Int, b: Int): Int = {
+    a - b
+  }
+
+  def multiplication(a: BigInt, b: BigInt) = a * b
+
+  def division(a: Double, b: Double): Double = {
+    a/b
+  }
+
+  def modulus(a: Int, b: Int): Int = {
+    a % b
+  }
 }
--- src/test/scala/harshida_shaily/TGTrainingTest.scala
+++ src/test/scala/harshida_shaily/TGTrainingTest.scala
@@ -1,8 +1,38 @@
-package harshida_shaily
+import harshida_shaily.TGTraining
+import org.scalatest.flatspec.AsyncFlatSpecLike
 
-import org.scalatest.flatspec.AnyFlatSpec
+class CalculatorTest extends AsyncFlatSpecLike {
 
-class TGTrainingTest extends AnyFlatSpec {
+  val mathFunctions=new TGTraining
 
-  behavior of "Welcome to Scala Test"
+  it should "add" in{
+    assert(mathFunctions.add(1,3)==4)
+  }
+
+  it should "subtract" in{
+    assert(mathFunctions.subtract(8,9)== -1)
+  }
+
+  it should "multiply" in{
+    assert(mathFunctions.multiplication(9,0)==0)
+  }
+
+  //Invoking the object of the Exception class
+  val divException=new divisionException
+
+  try {
+    it should "divide" in{
+      assert(mathFunctions.division(9.0,0.0)== 3.0)
+    }
+  }catch{
+    case divException=>;
+  }
+
+  it should "modulo" in{
+    assert(mathFunctions.modulus(10,5)==0)
+  }
+}
+
+class divisionException extends Exception{
+  println("The denominator is zero which is not possible. Hence your test case is failed")
 }
