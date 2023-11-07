4be74f4dcc20929fe4a286f846afdcf2dba29b80
Shubhang-2111 <singhshubhang1920@gmail.com>
2023-11-07 16:55:10 +0530

    Used class as per the feedback also used exception handling in divide case.

--- src/main/scala/shubhang/TGTraining.scala
+++ src/main/scala/shubhang/TGTraining.scala
@@ -1,6 +1,24 @@
 package shubhang
+import scala.util.{Success,Failure, Try}
 
-class TGTraining {
+class TGTraining(num1:Int,num2:Int) {
+  def add(): Int = {
+    num1 + num2
+  }
 
-  def welcome() = "Welcome to Scala"
+  def subtract(): Int = {
+    num1 - num2
+  }
+
+  def multiply(): Int = {
+    num1 * num2
+  }
+
+  def divide(): Any = {
+    val result = Try(num1 / num2)
+    result match {
+      case Success(value) => value
+      case Failure(exception) => Failure(exception)
+    }
+  }
 }
--- src/test/scala/shubhang/TGTrainingTest.scala
+++ src/test/scala/shubhang/TGTrainingTest.scala
@@ -1,8 +1,28 @@
 package shubhang
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
 
-class TGTrainingTest extends AnyFlatSpec {
+import scala.util.Failure
 
-  behavior of "Welcome to Scala Test"
+class TGTrainingTest extends AnyFunSuite {
+  test("TGTraining.add") {
+    assert(new TGTraining(3, 4).add() === 7)
+    assert(new TGTraining(-3, 9).add() === 6)
+  }
+
+  test("MathsOperation.subtract") {
+    assert(new TGTraining(5, 2).subtract() === 3)
+    assert(new TGTraining(-5, -2).subtract() === -3)
+  }
+
+  test("MathsOperation.multiply") {
+    assert(new TGTraining(5, 2).multiply() === 10)
+    assert(new TGTraining(-5, -7).multiply() === 35)
+  }
+
+  test("MathsOperation.divide") {
+    assert(new TGTraining(-500, -100).divide() == 5)
+    assert(new TGTraining(200, 0).divide().isInstanceOf[Failure[ArithmeticException]])
+  }
 }
+
