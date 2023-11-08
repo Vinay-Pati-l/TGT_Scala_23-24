9ca2e74a2ca853c14708a7e369a18cda557b56b5
Kushagra <kushagrathegame1@gmail.com>
2023-11-08 12:06:06 +0530

    Tweaked Assignment-1 with all the advised feedbacks.

--- src/main/scala/kushagra/CalculationOperations.scala
+++ src/main/scala/kushagra/CalculationOperations.scala
@@ -1,20 +1,26 @@
 package kushagra
+
 class CalculationOperations {
+
   def addition(num1: Long, num2: Long): Long = {
     return (num1 + num2)
   }
+
   def difference(num1: Long, num2: Long): Long = {
     return (num1 - num2)
   }
+
   def product(num1: Long, num2: Long): Long = {
     return (num1 * num2)
   }
+
+  // dividend is taken in Double to ensure the calculation happens
+  // in floating-point arithmetic (int case of Division)
   def division(dividend: Double, divisor: Int): Double = {
-    // dividend is taken in Double to ensure the calculation happens
-    // in floating-point arithmetic
     if (divisor == 0) throw new IllegalArgumentException("Division by zero is not allowed!!")
     return (dividend / divisor)
   }
+
   def find_remainder(dividend: Int, divisor: Int): Int = {
     return (dividend % divisor)
   }
