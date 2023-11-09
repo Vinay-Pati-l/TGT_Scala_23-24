e0fdb5b17faef350bc6ebc20454cf66a9019973a
Shubhang-2111 <singhshubhang1920@gmail.com>
2023-11-09 22:31:54 +0530

    Passed the params in function instead of class as per the feedback

--- src/test/scala/shubhang/TGTrainingTest.scala
+++ src/test/scala/shubhang/TGTrainingTest.scala
@@ -4,25 +4,25 @@ import org.scalatest.funsuite.AnyFunSuite
 
 
 class TGTrainingTest extends AnyFunSuite {
-  val trainingObject = new TGTraining
+  val tgtTrainingObject = new TGTraining
   test("TGTraining.add") {
-    assert(trainingObject.add(3,4) === 7)
-    assert(trainingObject.add(-3,9) === 6)
+    assert(tgtTrainingObject.add(3,4) === 7)
+    assert(tgtTrainingObject.add(-3,9) === 6)
   }
 
   test("TGTraining.subtract") {
-    assert(trainingObject.subtract(5,2) === 3)
-    assert(trainingObject.subtract(-5,-2) === -3)
+    assert(tgtTrainingObject.subtract(5,2) === 3)
+    assert(tgtTrainingObject.subtract(-5,-2) === -3)
   }
 
   test("TGTraining.multiply") {
-    assert(trainingObject.multiply(5,2) === 10)
-    assert(trainingObject.multiply(-5,-7) === 35)
+    assert(tgtTrainingObject.multiply(5,2) === 10)
+    assert(tgtTrainingObject.multiply(-5,-7) === 35)
   }
 
   test("TGTraining.divide") {
-    assert(trainingObject.divide(-500,-100) == 5)
-    assert(trainingObject.divide(200,0) == Int.MaxValue) // division with zero it gets an undefined value
+    assert(tgtTrainingObject.divide(-500,-100) == 5)
+    assert(tgtTrainingObject.divide(200,0) == Int.MaxValue) // division with zero it gets an undefined value
   }
 }
 
