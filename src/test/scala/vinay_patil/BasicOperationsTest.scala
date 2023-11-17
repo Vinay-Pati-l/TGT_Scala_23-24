70ad97bdefe2cbe281f501b77e7876c8455610a2
Vinay Pati l <vspatilkondapur@gmail.com>
2023-11-17 13:07:35 +0530

    Modified code as per desired changes

--- src/test/scala/vinay_patil/BasicOperationsTest.scala
+++ src/test/scala/vinay_patil/BasicOperationsTest.scala
@@ -3,53 +3,61 @@ package vinay_patil
 import org.scalatest.funsuite.AnyFunSuite
 
 class BasicOperationsTest extends AnyFunSuite{
-val Operator = new BasicOperations
-  test("BasicOperations.Add") {
-    assert(Operator.Add(5, 6)==11)
-    assert(Operator.Add(9.4, 4.5)==13.9)
-    assert(Operator.Add('a', 5)=='f')
-    assert(Operator.Add('A', 'B')==131)
-  }
-  test("BasicOperations.AddString"){
-    assert(Operator.AddString("Scala", "Practice")=="ScalaPractice")
-  }
-  test("BasicOperations.Subtract") {
-    assert(Operator.Subtract(5, 6) == -1)
-    assert(Operator.Subtract(9.4, 4.5) == 4.9)
-    assert(Operator.Subtract('A', 75) == -10)
-    assert(Operator.Subtract('A', 'B') == -1)
-  }
-  test("BasicOperations.Multiply") {
-    assert(Operator.Multiply(5, 6) == 30)
-    assert(Operator.Multiply(1.0, 4.5) == 4.5)
-    assert(Operator.Multiply('A', 5) == 325)
-    assert(Operator.Multiply('A', 'B') == 4290)
-  }
-  test("BasicOperations.MultiplyString") {
-    assert(Operator.MultiplyString("Scala", 2) == "ScalaScala")
-  }
-  test("Division With Option Exception Handling") {
-    assert(Operator.DivideWithOption(12, 6).contains(2))
-    assert(Operator.DivideWithOption(9.4, 4.5).contains(2.088888888888889))
-    assert(Operator.DivideWithOption('A', 5).contains(13))
-    assert(Operator.DivideWithOption('A', 'z').contains(0.5327868852459017))
-    assert(Operator.DivideWithOption('A', 0).isEmpty)
-  }
-  test("Division With Either Exception Handling"){
-    assert(Operator.DivideWithEither(12, 6)==Right(2))
-    assert(Operator.DivideWithEither(9.4, 4.5)==Right(2.088888888888889))
-    assert(Operator.DivideWithEither('A', 5)==Right(13))
-    assert(Operator.DivideWithEither('A', 'z')==Right(0.5327868852459017))
-    assert(Operator.DivideWithEither('A', 0)==Left("Cannot Divide by zero"))
-  }
-  test("Division With TryCatch Exception Handling") {
 
-    assert(Operator.DivideWithTry(12, 6) == 2)
-    assert(Operator.DivideWithTry(9.4, 4.5) == 2.088888888888889)
-    assert(Operator.DivideWithTry('A', 5) == 13)
-    assert(Operator.DivideWithTry('A', 'z') == 0.5327868852459017)
+val basicOperations = new BasicOperations
+
+  test("BasicOperations.Add"){
+    assert(basicOperations.Add(5, 6)==11)
+    assert(basicOperations.Add(9.4, 4.5)==13.9)
+    assert(basicOperations.Add('a', 5)=='f')
+    assert(basicOperations.Add('A', 'B')==131)
+  }
+
+  test("BasicOperations.AddString"){
+    assert(basicOperations.AddString("Scala", "Practice")=="ScalaPractice")
+  }
+
+  test("BasicOperations.Subtract"){
+    assert(basicOperations.Subtract(5, 6) == -1)
+    assert(basicOperations.Subtract(9.4, 4.5) == 4.9)
+    assert(basicOperations.Subtract('A', 75) == -10)
+    assert(basicOperations.Subtract('A', 'B') == -1)
+  }
+
+  test("BasicOperations.Multiply"){
+    assert(basicOperations.Multiply(5, 6) == 30)
+    assert(basicOperations.Multiply(1.0, 4.5) == 4.5)
+    assert(basicOperations.Multiply('A', 5) == 325)
+    assert(basicOperations.Multiply('A', 'B') == 4290)
+  }
+
+  test("BasicOperations.MultiplyString"){
+    assert(basicOperations.MultiplyString("Scala", 2) == "ScalaScala")
+  }
+
+  test("Division With Option Exception Handling"){
+    assert(basicOperations.DivideWithOption(12, 6).contains(2))
+    assert(basicOperations.DivideWithOption(9.4, 4.5).contains(2.088888888888889))
+    assert(basicOperations.DivideWithOption('A', 5).contains(13))
+    assert(basicOperations.DivideWithOption('A', 'z').contains(0.5327868852459017))
+    assert(basicOperations.DivideWithOption('A', 0).isEmpty)
+  }
+
+  test("Division With Either Exception Handling"){
+    assert(basicOperations.DivideWithEither(12, 6)==Right(2))
+    assert(basicOperations.DivideWithEither(9.4, 4.5)==Right(2.088888888888889))
+    assert(basicOperations.DivideWithEither('A', 5)==Right(13))
+    assert(basicOperations.DivideWithEither('A', 'z')==Right(0.5327868852459017))
+    assert(basicOperations.DivideWithEither('A', 0)==Left("Cannot Divide by zero"))
+  }
+
+  test("Division With TryCatch Exception Handling"){
+    assert(basicOperations.DivideWithTry(12, 6) == 2)
+    assert(basicOperations.DivideWithTry(9.4, 4.5) == 2.088888888888889)
+    assert(basicOperations.DivideWithTry('A', 5) == 13)
+    assert(basicOperations.DivideWithTry('A', 'z') == 0.5327868852459017)
     try {
-      Operator.DivideWithTry('A', 0)
+      basicOperations.DivideWithTry('A', 0)
     }
     catch {
       case e : ArithmeticException=> println(e.printStackTrace())
