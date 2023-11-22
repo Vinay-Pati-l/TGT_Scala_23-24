b939feb50a8965ea22429432fa8432961aa3980e
Abhijeet Gaurav <abhijeetgaurav800@gmail.com>
2023-11-22 18:22:53 +0530

    second commi

--- src/main/scala/abhijeet_gaurav/Palindrome.scala
+++ src/main/scala/abhijeet_gaurav/Palindrome.scala
@@ -1,26 +1,14 @@
 package abhijeet_gaurav
 
-class Palindrome {
+object Palindrome extends App {
   def isPalindrome(st: String): Boolean = {
     val len = st.length
-
     for (i <- 0 until len / 2) {
       if (st(i) != st(len - i - 1))
         return false
     }
 
     true
-  }
-}
-
-object PalindromeApp extends App {
-  val str = "aaA"
-  val palindromeObj = new Palindrome
-
-  if (palindromeObj.isPalindrome(str)) {
-    println(s"$str is a palindrome.")
-  } else {
-    println(s"$str is not a palindrome.")
   }
 }
 
